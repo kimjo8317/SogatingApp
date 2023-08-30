b5a78e70aa8819eef70aad6af9ae9d920b94094d
kimjunoh <kimjo8317@gmail.com>
2023-08-30 21:01:51 +0900

    splash,intro screen

---
+++ app/src/main/java/com/cokchi/sogating_final/auth/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.cokchi.sogating_final.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.cokchi.sogating_final.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
