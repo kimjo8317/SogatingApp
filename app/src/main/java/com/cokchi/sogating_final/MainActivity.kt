a4eddf2f64b8da2fd377005c0d269f7f578e5bb7
kimjunoh <kimjo8317@gmail.com>
2023-08-31 17:40:54 +0900

    logout 기능추가

--- app/src/main/java/com/cokchi/sogating_final/MainActivity.kt
+++ app/src/main/java/com/cokchi/sogating_final/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.cokchi.sogating_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import com.cokchi.sogating_final.auth.IntroActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -16,6 +21,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //로그아웃기능
+        val setting = findViewById<ImageView>(R.id.settingIcon)
+        setting.setOnClickListener {
+            val auth = Firebase.auth
+            auth.signOut()
+
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+        }
 
         val cardStackView = findViewById<CardStackView>(R.id.cardStackView)
 
