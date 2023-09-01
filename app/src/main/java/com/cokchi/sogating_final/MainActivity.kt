13796cba4759f71c0a219ed7fbfaa4cfd11fd2c3
kimjo8317 <kimjo8317@naver.com>
2023-09-01 23:58:27 +0900

    좋아요기능

--- app/src/main/java/com/cokchi/sogating_final/MainActivity.kt
+++ app/src/main/java/com/cokchi/sogating_final/MainActivity.kt
@@ -7,15 +7,13 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
-import com.cokchi.sogating_final.auth.IntroActivity
 import com.cokchi.sogating_final.auth.UserDataModel
 import com.cokchi.sogating_final.setting.SettingActivity
+import com.cokchi.sogating_final.utils.FirebaseAuthUtils
 import com.cokchi.sogating_final.utils.FirebaseRef
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.ktx.Firebase
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -23,19 +21,26 @@ import com.yuyakaido.android.cardstackview.Direction
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var CardStackAdapter : CardStackAdapter
-    lateinit var manager : CardStackLayoutManager
+    lateinit var cardStackAdapter: CardStackAdapter
+    lateinit var manager: CardStackLayoutManager
 
     private val TAG = "MainActivity"
 
-    private val userDataList = mutableListOf<UserDataModel>()
+    private val usersDataList = mutableListOf<UserDataModel>()
 
     private var userCount = 0
 
+    private lateinit var currentUserGender: String
+
+    private val uid = FirebaseAuthUtils.getUid()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 다른 성별 유저받아오기
+        // 1. 나의 성별을 알고
+        // 2. 전체 유저 중 다른 성별 가져오기
 
         val setting = findViewById<ImageView>(R.id.settingIcon)
         setting.setOnClickListener {
@@ -55,6 +60,10 @@ class MainActivity : AppCompatActivity() {
 
                 if (direction == Direction.Right) {
                     Toast.makeText(this@MainActivity, "right", Toast.LENGTH_SHORT).show()
+                    //현재유저의 UID값을 로그찍어보기
+                    Log.d(TAG, usersDataList[userCount].uid.toString())
+
+                    userLikeOtherUser(uid, usersDataList[userCount].uid.toString())
                 }
                 if (direction == Direction.Left) {
                     Toast.makeText(this@MainActivity, "left", Toast.LENGTH_SHORT).show()
@@ -62,13 +71,11 @@ class MainActivity : AppCompatActivity() {
 
                 userCount = userCount + 1
 
-                if (userCount == userDataList.count()) {
-                    getUserDataList()
-                    Toast.makeText(this@MainActivity, "유저를 새롭게 받아오는중", Toast.LENGTH_LONG)
+                if (userCount == usersDataList.count()) {
+                    getUserDataList(currentUserGender)
+                    Toast.makeText(this@MainActivity, "유저를 새롭게 받아오는중", Toast.LENGTH_LONG).show()
                 }
             }
-
-
 
             override fun onCardRewound() {
 
@@ -85,31 +92,29 @@ class MainActivity : AppCompatActivity() {
             override fun onCardDisappeared(view: View?, position: Int) {
 
             }
-
         })
 
-
-        CardStackAdapter = CardStackAdapter(baseContext, userDataList)
+        cardStackAdapter = CardStackAdapter(baseContext, usersDataList)
         cardStackView.layoutManager = manager
-        cardStackView.adapter = CardStackAdapter
+        cardStackView.adapter = cardStackAdapter
 
-        //유저데이터를 리스트에 담음
-        getUserDataList()
-
+        // 유저 데이터를 리스트에 담음
+        // getUserDataList()
+        // 나의 성별 가져와서 담기
+        getMyUserData()
     }
 
-    //회원정보 받아오기
-    private fun getUserDataList(){
-
+    private fun getMyUserData() {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for (dataModel in dataSnapshot.children) {
+                Log.d(TAG, dataSnapshot.toString())
+                val data = dataSnapshot.getValue(UserDataModel::class.java)
 
-                    val user = dataModel.getValue(UserDataModel::class.java)
-                    userDataList.add(user!!)
-                }
-                //데이터를 받아온뒤 다시한번 동기화
-                CardStackAdapter.notifyDataSetChanged()
+                Log.d(TAG, data?.gender.toString())
+
+                currentUserGender = data?.gender.toString()
+
+                getUserDataList(currentUserGender)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -117,7 +122,41 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
             }
         }
-        FirebaseRef.userInfoRef.addValueEventListener(postListener)
 
+        // 파이어베이스 안에 UID 정보를 가져옴
+        FirebaseRef.userInfoRef.child(uid).addListenerForSingleValueEvent(postListener)
+    }
+
+    // 회원 정보 받아오기
+    private fun getUserDataList(currentUserGender: String) {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (dataModel in dataSnapshot.children) {
+                    val user = dataModel.getValue(UserDataModel::class.java)
+
+                    // 성별이 현재 유저의 성별과 같을 때와 (if) 다를 때 (else)
+                    if (user!!.gender.toString().equals(currentUserGender)) {
+                    } else {
+                        usersDataList.add(user!!)
+                    }
+                }
+
+                // 데이터를 받아온 뒤 다시 한 번 동기화
+                cardStackAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+
+        FirebaseRef.userInfoRef.addValueEventListener(postListener)
+    }
+
+    //유저의 좋아요를 표시하는 로직
+    //나의 UID 값, 좋아요한 사람의 UID값
+    private fun userLikeOtherUser(myUid : String, otherUid : String) {
+        FirebaseRef.userLikeRef.child(myUid).child(otherUid).setValue("true")
     }
 }
